ea468740699513295cc67c9c7d5f1ea3571f046d
Masood ul Rehman <masooddulrehman@gmail.com>
2024-05-25 17:14:31 +0500

    updated resume data

--- src/data/resume-data.tsx
+++ src/data/resume-data.tsx
@@ -15,9 +15,9 @@ export const RESUME_DATA = {
   location: "Multan, Pakistan",
   locationLink: "https://www.google.com/maps/place/Multan",
   about:
-    "Frontend expert focused on building products with extra attention to detail",
+    "Full-stack Engineer focused on building products with extra attention to detail",
   summary:
-    "As a Frontend developer I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. At my last job at The Tech Square I was the lead frontend developer and I worked with  other team members to complete the project effectively.  I work mostly with TypeScript, React, Node js , React query, Node.js, and express. I have in total 3 years of experience in working remotely with companies all around the world.",
+    "As a Full-stack developer I have successfully taken multiple products from 0 to 1. I lead teams effectively, ensuring an environment where people can do their best work. At my last job at The Tech Square I was the lead frontend developer and I worked with  other team members to complete the project effectively.  I work mostly with TypeScript, React, Node js , React query, Node.js, and express. I have in total 3 years of experience in working remotely with companies all around the world.",
   avatarUrl:
     "https://avatars.githubusercontent.com/u/81099787?s=400&u=8b977a8af7375ec29ce5529fd90e29c78f2a3238&v=4",
   personalWebsiteUrl: "https://masoodulrehman.com",
@@ -37,7 +37,7 @@ export const RESUME_DATA = {
       },
       {
         name: "X",
-        url: "https://x.com/MasoodulRehman4",
+        url: "https://x.com/i_am_Masood_",
         icon: XIcon,
       },
     ],
@@ -55,8 +55,7 @@ export const RESUME_DATA = {
       company: "Upwork",
       link: "https://upwork.com",
       badges: ["Remote"],
-      title: "Frontend developer and designer",
-      logo: ClevertechLogo,
+      title: "Full-stack developer and designer",
       start: "2019",
       end: "Current",
       description:
@@ -70,16 +69,50 @@ export const RESUME_DATA = {
     "Node.js",
     "React query",
     "express",
+    "python",
   ],
   projects: [
     {
-      title: "Shopify clone",
+      title: "Bamrec Super Admin",
+      techStack: [
+        "React",
+        "React query",
+        "tailwind css",
+        "Node js",
+        "Mongodb",
+        "Fastify",
+      ],
+      description:
+        "A super admin dashboard which I worked on at The Tech Square. Bamrec is the event company which allows organization to create events, superadmin can view the events running its details and more. I was the lead frontend developer on this project and worked on few apis as well.",
+      link: {
+        label: "rekt.news",
+        href: "https://dev.superadmin.bamrec.com/",
+      },
+    },
+    {
+      title: "Rekt News Feed",
+      techStack: [
+        "Python",
+        "flask",
+        "redis",
+        "flask",
+        "mongodb",
+        "react",
+        "Next.js",
+      ],
+      description:
+        "A rekt upcoming feature that will allow users to see the latest news and updates related to crypto from top crypto enthusiasts. This will be a feed that will be updated after 24 hours and will be available to all users.",
+      link: {
+        label: "rekt.news",
+        href: "https://rekt.news/leaderboard/",
+      },
+    },
+    {
+      title: "Shopify Clone",
       techStack: ["Final year project", "React", "Redux", "express", "node js"],
       description:
         "A platform to build and ecommerce stores. User can create multiple stores and add delte products in particular stores.",
-      logo: ConsultlyLogo,
       link: {
-        label: "consultly.com",
         href: "https://github.com/Masood-ul-Rehman/ShopifyFYP",
       },
     },
@@ -119,17 +152,6 @@ export const RESUME_DATA = {
       link: {
         label: "Merchvisor",
         href: "https://www.behance.net/gallery/159474973/Mechvisor-Landing-page-design",
-      },
-    },
-    {
-      title: "LinkTree clone",
-      techStack: ["Side Project", "Next.js", "postgress sql", "in progress"],
-      description:
-        "Clone of Linktree a popular application for showing your applications",
-      logo: Minimal,
-      link: {
-        label: "useminimal.com",
-        href: "https://links-nbw3dqepo-masood-ul-rehman.vercel.app/",
       },
     },
   ],
